e125239b32b089c6788a54b5d72702b5f25a7b87
y9828 <y9828@naver.com>
2026-01-31 22:22:08 +0900

    [생성] springMyBatisFood 폴더 생성

---
+++ springMyBatisFood/src/main/java/com/zeus/SpringMyBatisFoodApplication.java
@@ -0,0 +1,13 @@
+package com.zeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyBatisFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyBatisFoodApplication.class, args);
+	}
+
+}
